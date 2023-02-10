6b2c43602f515f12d062153c02733f32dbe2deaf
Youngho Cho <dominoyh5@gmail.com>
2023-02-10 19:34:20 +0900

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <slot></slot>
+
+<footer class="footer">
+    <div class="content has-text-centered">
+        <strong>Exercise diary</strong> by <a href="https://github.com/cho0h5">cho0h5</a>.
+        <br/>
+        You can see this <a href="https://github.com/cho0h5/exercise-diary">repository</a> on GitHub.
+        <br/>
+        Thank you, <a href="https://www.youtube.com/@masterchoi_">Master Choi</a>.
+    </div>
+</footer>
